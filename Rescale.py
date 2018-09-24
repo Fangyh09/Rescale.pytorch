8d9daf2d85288d8c18f277ec3759ab8eb24b3bfa
Coder_Hong <fangyh09@gmail.com>
2018-09-24 21:16:34 +0800

    Create Rescale.py

---
+++ Rescale.py
@@ -0,0 +1,25 @@
+class Rescale(object):
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image_, pose_ = sample['image'], sample['pose']
+        h, w = image_.shape[:2]
+        im_scale = min(float(self.output_size[0]) / float(h), float(self.output_size[1]) / float(w))
+        new_h = int(image_.shape[0] * im_scale)
+        new_w = int(image_.shape[1] * im_scale)
+        image = cv2.resize(image_, (new_w, new_h),
+                    interpolation=cv2.INTER_LINEAR)
+        left_pad = (self.output_size[1] - new_w) // 2
+        right_pad = (self.output_size[1] - new_w) - left_pad
+        top_pad = (self.output_size[0] - new_h) // 2
+        bottom_pad = (self.output_size[0] - new_h) - top_pad
+        mean=np.array([0.485, 0.456, 0.406]) * 256
+        pad = ((top_pad, bottom_pad), (left_pad, right_pad))
+        image = np.stack([np.pad(image[:,:,c], pad, mode='constant', constant_values=mean[c]) 
+                        for c in range(3)], axis=2)
+        pose = (pose_.reshape([-1,2])/np.array([w,h])*np.array([new_w,new_h]))
+        pose += [left_pad, top_pad]
+        pose = pose.flatten()
+        return {'image': image, 'pose': pose}
